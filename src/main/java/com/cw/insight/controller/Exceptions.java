f05d0845b99eac55d51e907d1035c6265734f48b
Dont <123>
2018-08-05 18:55:56 +0800

    exception提交

--- src/main/java/com/cw/insight/controller/Exceptions.java
+++ src/main/java/com/cw/insight/controller/Exceptions.java
@@ -1,8 +1,12 @@
 package com.cw.insight.controller;
 
+import com.cw.insight.utils.DbTools;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.ResultSet;
 
 @RestController
 public class Exceptions {
@@ -11,103 +15,114 @@ public class Exceptions {
     获取未处理的异常信息列表。
      */
     @RequestMapping("/unhandledexceptions")
-    public String getUnhandledExceptions(){
-        return "{\n" +
-                "    \"records\": [{\n" +
-                "        \"id\":\"e0001\",\n" +
-                "        \"title\":\"代理业务对账异常\",\n" +
-                "        \"app\":\"GCMC\"\n" +
-                "    }, {\n" +
-                "        \"id\":\"e0002\",\n" +
-                "        \"title\":\"日终批量中断\",\n" +
-                "        \"app\":\"CAPS\"\n" +
-                "    }, {\n" +
-                "        \"id\":\"e0003\",\n" +
-                "        \"title\":\"指令处理成功率异常\",\n" +
-                "        \"app\":\"CAPS\"\n" +
-                "    }, {\n" +
-                "        \"id\":\"e0004\",\n" +
-                "        \"title\":\"收款重复处理报错\",\n" +
-                "        \"app\":\"GCMC\"\n" +
-                "    }, {\n" +
-                "        \"id\":\"e0005\",\n" +
-                "        \"title\":\"他行支付调用失败\",\n" +
-                "        \"app\":\"MBPS\"\n" +
-                "    }]\n" +
-                "}";
-    }
-
-    /*
-    获取未处理的异常详细。
-     */
-    @RequestMapping("/exception/{exid}")
-    public String getExceptionById(@PathVariable String exid){
-        return "{\n" +
-                "    \"app\":{\n" +
-                "        \"name\":\"CAPS\",\n" +
-                "        \"id\":\"CAPS\"\n" +
-                "    },\n" +
-                "    \"description\":{\n" +
-                "        \"title\":\"出大事啦\",\n" +
-                "        \"content\":\"我也不知道怎么回事支付就出现异常了，昨天还好好的，(┬＿┬)\",\n" +
-                "        \"time\":\"2017-10-12 18:01\"\n" +
-                "    },\n" +
-                "    \"solution\":{\n" +
-                "        \"content\":\"我也不知道怎么回事支付就出现异常了，昨天还好好的，(┬＿┬)\",\n" +
-                "        \"time\":\"2017-10-12 18:01\"\n" +
-                "    }\n" +
-                "}";
+    public String getUnhandledExceptions() {
+        String exceptions = "";
+        try {
+            ResultSet rs = DbTools.doQuery("select * from insight_app_exception where is_handled='1' order by des_time desc");
+            exceptions = "{\"records\":[";
+            while (rs.next()) {
+                exceptions += "{\"id\":\"" + rs.getString("exid") + "\"," +
+                        "\"title\":\"" + rs.getString("title") + "\"," +
+                        "\"app\":\"" + rs.getString("appid") + "\"},";
+            }
+            if (",".equals(exceptions.substring(exceptions.length() - 1))) {
+                exceptions = exceptions.substring(0, exceptions.length() - 1);
+            }
+            exceptions += "]}";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return exceptions;
     }
 
     /*
     获取已处理的异常信息列表。
      */
     @RequestMapping("/handledexceptions")
-    public String getHandledExceptions(){
-        return "{\n" +
-                "    \"page\": 0,\n" +
-                "    \"records\":[\n" +
-                "        {\n" +
-                "            \"date\":\"2018-07-01\",\n" +
-                "            \"exceptions\":[\n" +
-                "                {\n" +
-                "                    \"time\":\"19:02\",\n" +
-                "                    \"title\":\"代理业务对账异常\",\n" +
-                "                    \"app\":\"GCMC\"\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"time\":\"19:02\",\n" +
-                "                    \"title\":\"代理业务对账异常\",\n" +
-                "                    \"app\":\"GCMC\"\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"time\":\"19:02\",\n" +
-                "                    \"title\":\"代理业务对账异常\",\n" +
-                "                    \"app\":\"GCMC\"\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"date\":\"2017-08-21\",\n" +
-                "            \"exceptions\":[\n" +
-                "                {\n" +
-                "                    \"time\":\"19:02\",\n" +
-                "                    \"title\":\"代理业务对账异常\",\n" +
-                "                    \"app\":\"GCMC\"\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"time\":\"19:02\",\n" +
-                "                    \"title\":\"代理业务对账异常\",\n" +
-                "                    \"app\":\"GCMC\"\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"time\":\"19:02\",\n" +
-                "                    \"title\":\"代理业务对账异常\",\n" +
-                "                    \"app\":\"GCMC\"\n" +
-                "                }\n" +
-                "            ]\n" +
-                "        }\n" +
-                "    ]\n" +
-                "}";
+    public String getHandledExceptions(HttpServletRequest request) {
+        String page = request.getParameter("page");
+        int offset = Integer.parseInt(page) * 10;
+        String exceptions = "{\"page\":\"" + page + "\",";
+        String dateTemp = "";
+        try {
+            ResultSet rs = DbTools.doQuery("select * from insight_app_exception where is_handled='1' order by des_time desc limit " + offset + ",10");
+            exceptions += "\"records\":[";
+            while (rs.next()) {
+                String datePart = rs.getString("des_time").substring(0, 10);
+                if (dateTemp.equals(datePart)) {//相同日期放在同一个节点
+                    exceptions += "{\"id\":\"" + rs.getString("exid") + "\"," +
+                            "\"time\":\"" + rs.getString("des_time").substring(11, 16) + "\"," +
+                            "\"title\":\"" + rs.getString("title") + "\"," +
+                            "\"app\":\"" + rs.getString("appid") + "\"},";
+                } else {
+                    if (!dateTemp.equals("")) {
+                        if (",".equals(exceptions.substring(exceptions.length() - 1))) {
+                            exceptions = exceptions.substring(0, exceptions.length() - 1);
+                        }
+                        exceptions += "]},";
+                    }
+                    exceptions += "{" +
+                            "\"date\":\"" + datePart + "\"," +
+                            "\"exceptions\":[" +
+                            "{\"id\":\"" + rs.getString("exid") + "\"" +
+                            "\"time\":\"" + rs.getString("des_time").substring(11, 16) + "\"," +
+                            "\"title\":\"" + rs.getString("title") + "\"," +
+                            "\"app\":\"" + rs.getString("appid") + "\"" +
+                            "},";
+                }
+                dateTemp = datePart;
+            }
+            if (",".equals(exceptions.substring(exceptions.length() - 1))) {
+                exceptions = exceptions.substring(0, exceptions.length() - 1);
+            }
+            exceptions += "]}]}";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return exceptions;
+    }
+
+    /*
+    获取未处理的异常详细。
+     */
+    @RequestMapping("/exception/{exid}")
+    public String getExceptionById(@PathVariable String exid) {
+        String exception = "{}";
+        try {
+            ResultSet rs = DbTools.doQuery("select * from insight_app_exception where exid='" + exid + "'");
+            if (rs.next()) {
+                String app = "\"app\":{\"name\":\"" + rs.getString("appid") + "\",\"id\":\"" + rs.getString("appid") + "\"}";
+                String description = "\"description\":{\"title\":\"" + rs.getString("title") + "\",\"content\":\"" + rs.getString("description") + "\",\"time\":\"" + rs.getString("des_time") + "\"}";
+                String solution = "\"solution\":{\"content\":\"" + rs.getString("solution") + "\",\"time\":\"" + rs.getString("sol_time") + "\"}";
+                exception = "{" + app + "," + description + "," + solution + "}";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return exception;
+    }
+
+    @RequestMapping("/msg/warning")
+    public void doWarning(HttpServletRequest request) {
+        String appid = request.getParameter("appid");
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        String time = request.getParameter("time");
+        String customid = request.getParameter("customid");
+        String sql = "insert into insight_app_exception(exid,appid,title,description,des_time,solution,sol_time,is_handled,customid) " +
+                "values(unix_timestamp(now()),'"+appid+"','"+title+"','"+content+"','"+time+"','','','0','"+customid+"')";
+        DbTools.doUpdate(sql);
+        //http://localhost:8080/msg/warning?appid=TEST&title=titletitle&content=contentcontent&time=2018-12-01 11:11:11&customid=customidcustomid
+    }
+
+    @RequestMapping("/msg/finished")
+    public void doFinished(HttpServletRequest request) {
+        String appid = request.getParameter("appid");
+        String content = request.getParameter("content");
+        String time = request.getParameter("time");
+        String customid = request.getParameter("customid");
+        String sql = "update insight_app_exception set solution='"+content+"',sol_time='"+time+"',is_handled='1' where appid='"+appid+"' and customid='"+customid+"'";
+        DbTools.doUpdate(sql);
+        //http://localhost:8080/msg/finished?appid=TEST&content=ccccccc&time=2018-11-11 11:11:11&customid=customidcustomid
     }
 }
--- src/main/java/com/cw/insight/controller/Test.java
+++ src/main/java/com/cw/insight/controller/Test.java
@@ -15,47 +15,47 @@ public class Test {
         return Params.getParamById("appid");
     }
 
-    @RequestMapping("/template")
-    public String setTemplate(){
-        String appid = "wx1044da5d38d4d77d";
-        String secret = "69c5ee2b5b8bb56aba2aff32e8935bfe";
-        String template_id = "ST-eqvgOuAVHq-w4DYMuD4Uc0oITPJp9m3rQbl8ObM8";
-        String openid = "oZwF75Z-BtareG5p1jDzzLenCV5g";
-        String formid = "1533175450568";
-
-        String access_token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret+"";
-        String jsonStr = "";
-        try {
-            jsonStr = HttpTools.httpRequest(access_token_url,"GET",null);
-            java.lang.System.out.println("access_token_url="+jsonStr);
-            String access_token = "";
-            JSONObject jsonObject = (JSONObject) JSONValue.parseStrict(jsonStr);
-            if(jsonObject != null){
-                access_token = jsonObject.get("access_token") == null? "":jsonObject.get("access_token").toString();
-            }
-            java.lang.System.out.println("access_token="+access_token);
-            String template_url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token="+access_token;
-            //template_url += "&touser="+openid+"&template_id="+template_id+"&form_id="+formid+"&data={}";
-
-            String put = "{\n" +
-                    "  \"touser\": \""+openid+"\",\n" +
-                    "  \"template_id\": \""+template_id+"\",\n" +
-                    "  \"form_id\": \""+formid+"\",\n" +
-                    "  \"data\": {\n" +
-                    "      \"keyword1\": {\n" +
-                    "          \"value\": \"登录成功了喂\"\n" +
-                    "      },\n" +
-                    "      \"keyword2\": {\n" +
-                    "          \"value\": \"就是这里了\"\n" +
-                    "      }\n" +
-                    "  }\n" +
-                    "}";
-            jsonStr = HttpTools.httpRequest(template_url,"POST",put);
-            java.lang.System.out.println("template_url="+jsonStr);
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return jsonStr;
-    }
+//    @RequestMapping("/template")
+//    public String setTemplate(){
+//        String appid = "wx1044da5d38d4d77d";
+//        String secret = "69c5ee2b5b8bb56aba2aff32e8935bfe";
+//        String template_id = "ST-eqvgOuAVHq-w4DYMuD4Uc0oITPJp9m3rQbl8ObM8";
+//        String openid = "oZwF75Z-BtareG5p1jDzzLenCV5g";
+//        String formid = "1533175450568";
+//
+//        String access_token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret+"";
+//        String jsonStr = "";
+//        try {
+//            jsonStr = HttpTools.httpRequest(access_token_url,"GET",null);
+//            java.lang.System.out.println("access_token_url="+jsonStr);
+//            String access_token = "";
+//            JSONObject jsonObject = (JSONObject) JSONValue.parseStrict(jsonStr);
+//            if(jsonObject != null){
+//                access_token = jsonObject.get("access_token") == null? "":jsonObject.get("access_token").toString();
+//            }
+//            java.lang.System.out.println("access_token="+access_token);
+//            String template_url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token="+access_token;
+//            //template_url += "&touser="+openid+"&template_id="+template_id+"&form_id="+formid+"&data={}";
+//
+//            String put = "{\n" +
+//                    "  \"touser\": \""+openid+"\",\n" +
+//                    "  \"template_id\": \""+template_id+"\",\n" +
+//                    "  \"form_id\": \""+formid+"\",\n" +
+//                    "  \"data\": {\n" +
+//                    "      \"keyword1\": {\n" +
+//                    "          \"value\": \"登录成功了喂\"\n" +
+//                    "      },\n" +
+//                    "      \"keyword2\": {\n" +
+//                    "          \"value\": \"就是这里了\"\n" +
+//                    "      }\n" +
+//                    "  }\n" +
+//                    "}";
+//            jsonStr = HttpTools.httpRequest(template_url,"POST",put);
+//            java.lang.System.out.println("template_url="+jsonStr);
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        return jsonStr;
+//    }
 }
